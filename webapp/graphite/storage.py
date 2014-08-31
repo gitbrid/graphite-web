35a195129c828f858f12505cdde34753ec2bef3c
Denis Zhdanov <denis.zhdanov@gmail.com>
2014-08-31 23:21:54 +0200

    Backport of pr #710 to 0.9.x branch

--- webapp/graphite/storage.py
+++ webapp/graphite/storage.py
@@ -413,8 +413,8 @@ class RRDDataSource(Leaf):
     endString = time.strftime("%H:%M_%Y%m%d+%Ss", time.localtime(endTime))
 
     if settings.FLUSHRRDCACHED:
-      rrdtool.flushcached(self.fs_path, '--daemon', settings.FLUSHRRDCACHED)
-    (timeInfo,columns,rows) = rrdtool.fetch(self.fs_path,'AVERAGE','-s' + startString,'-e' + endString)
+      rrdtool.flushcached(RRDFile._convert_fs_path(self.fs_path), '--daemon', settings.FLUSHRRDCACHED)
+    (timeInfo,columns,rows) = rrdtool.fetch(RRDFile._convert_fs_path(self.fs_path),'AVERAGE','-s' + startString,'-e' + endString)
     colIndex = list(columns).index(self.name)
     rows.pop() #chop off the latest value because RRD returns crazy last values sometimes
     values = (row[colIndex] for row in rows)
